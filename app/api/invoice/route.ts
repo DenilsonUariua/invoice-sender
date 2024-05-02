666cb0bd226b218e5fecfad23b5bd4aa201c9f0c
Denilson Uariua <denuariua@gmail.com>
2024-05-02 08:30:06 +0200

    fixed not allowed issue

--- app/api/invoice/route.ts
+++ app/api/invoice/route.ts
@@ -3,15 +3,15 @@ import { EmailTemplate } from "../../components/email-templates/eplan-invoice";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
+export async function GET() {
   try {
     const data = await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
       to: ["denuariua@gmail.com"],
       subject: "Hello world",
       text: "Hello",
-      react: EmailTemplate({ firstName: "John" }) as any,
-    }) ;
+      react: EmailTemplate({ firstName: "John" }),
+    });
 
     return Response.json(data);
   } catch (error) {
