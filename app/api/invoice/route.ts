c858627b1645551948807a49816135c8e565bf34
Denilson Uariua <denuariua@gmail.com>
2024-05-03 22:43:34 +0200

    heello

--- app/api/invoice/route.ts
+++ app/api/invoice/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import { EmailTemplate } from "../../components/email-templates/eplan-invoice";
 // Nextjs route segment config
-export const dynamic = 'force-dynamic' // Force dynamic (server) route instead of static page
+export const dynamic = "force-dynamic"; // Force dynamic (server) route instead of static page
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function GET() {
@@ -9,8 +9,8 @@ export async function GET() {
     const data = await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
       to: ["denuariua@gmail.com"],
-      subject: "Hello world",
-      text: "Hello",
+      subject: `Denilson Uariua E-Plan invoice - ${new Date().getDay()}`,
+      text: "Hello Denilson this is the invoice that you created good work buddy keep on trucking.",
       react: EmailTemplate({ firstName: "John" }),
     });
 
